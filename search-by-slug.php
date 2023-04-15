1c3eb8a9ea295972cea6ab6a74de1566580ed334
WalkerKharkov <energaleks@gmail.com>
2023-04-15 15:20:56 +0300

    Create search-by-slug.php

---
+++ search-by-slug.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+    Plugin Name: Search by Slug
+    Description: This plugin allows you to search for a post or page by its slug
+    Author: ElDiablo
+    Version: 1.0.0
+*/
+
+const SBS_SEARCH_COMMAND = 'slug:';
+
+if ( ! function_exists( 'sbs_search_by_slug_query_adjustment' ) ) {
+    function sbs_search_by_slug_query_adjustment( $query ) {
+        // works in wp_admin area only
+        if ( ! is_admin() ) return;
+
+        // works on the edit.php page only
+        global $pagenow;
+
+        if ( $pagenow !== 'edit.php' ) return;
+
+        $search_string = $query->get( 's' );
+
+        if ( empty( $search_string ) ) return;
+
+        // checks if search by slug activated
+        if ( strpos( trim( $search_string ), SBS_SEARCH_COMMAND ) !== 0 ) return;
+
+        $slug = trim( str_replace( SBS_SEARCH_COMMAND, '', $search_string ) );
+        $query->set( 's', '' );
+        $query->set( 'sbs_slug_search', $slug );
+    }
+
+    add_action( 'pre_get_posts', 'sbs_search_by_slug_query_adjustment' );
+}
+
+if ( ! function_exists( 'sbs_search_by_slug_request_adjustment' ) ) {
+    function sbs_search_by_slug_request_adjustment( $request, WP_Query $query ) {
+        if ( isset( $query->query_vars['sbs_slug_search'] ) && ! empty( $query->query_vars['sbs_slug_search'] ) ) {
+            global $wpdb;
+            $search_string = 'WHERE 1=1  AND ((' . $wpdb->posts . '.post_name LIKE "%' . $query->query_vars['sbs_slug_search'] . '%") AND ';
+            $request = str_replace('WHERE 1=1  AND (', $search_string, $request );
+        }
+
+        return $request;
+    }
+
+    add_filter( 'posts_request', 'sbs_search_by_slug_request_adjustment', 10, 2 );
+}
+
